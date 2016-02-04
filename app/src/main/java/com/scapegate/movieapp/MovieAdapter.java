6924c265d44547d70c72fea6ef95113305884e8c
Mher <poghosyanUSC@gmail.com>
2016-02-04 11:50:50 -0800

    Added ApiCall class - now able to obtain JSON data by sending http get api requests

--- app/src/main/java/com/scapegate/movieapp/MovieAdapter.java
+++ app/src/main/java/com/scapegate/movieapp/MovieAdapter.java
@@ -1,49 +1,41 @@
 package com.scapegate.movieapp;
 
-import android.content.Context;
+import android.app.Activity;
+import android.graphics.Point;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
 
 /**
  * Created by Mher on 1/27/2016.
  */
-public class MovieAdapter extends BaseAdapter {
-    int size;
-    Context c;
+public class MovieAdapter extends ArrayAdapter<String> {
 
-    public MovieAdapter(Context context, int boxSize) {
-        c = context;
-        size = boxSize;
+    public MovieAdapter(Activity context, List<String> imageUrls) {
+        super(context, 0, imageUrls);
     }
 
-    @Override
-    public int getCount() {
-        return 10;
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        String url = getItem(position);
         ImageView imageView;
         if(convertView == null) {
-            convertView = LayoutInflater.from(c).inflate(R.layout.movie_item, parent, false);
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.movie_item, parent, false);
         }
+        Point size = new Point();
+        Activity act = (Activity) getContext();
+        act.getWindowManager().getDefaultDisplay().getSize(size);
         imageView = (ImageView) convertView.findViewById(R.id.movie_poster);
-        imageView.setLayoutParams(new GridView.LayoutParams(size, size));
-        imageView.setImageResource(R.drawable.sample_0);
+        imageView.setLayoutParams(new GridView.LayoutParams(size.x/2,size.x/2));
+        Picasso.with(getContext()).load(url).into(imageView);
         return imageView;
     }
 }
